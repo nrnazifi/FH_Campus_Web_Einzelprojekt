ee83c50a886cccea5279e9d2dd86fc5c84154399
Navid <navid.nazifi@gmail.com>
2021-09-25 22:50:12 +0200

    add fileloader to show images in html

--- src/api/main.js
+++ src/api/main.js
@@ -2,4 +2,9 @@
 import bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
-import '../assets/styles/main.css'
+
+// custom css
+import '../assets/styles/main.css';
+
+// images
+import img from '../assets/images/logo.png';
--- webpack.common.js
+++ webpack.common.js
@@ -23,9 +23,21 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     'style-loader',
-                    'css-loader'
+                    'css-loader',
                 ]
-            }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: './assets/images/'
+                          }
+                    },
+                ],
+            },
         ]
     },
     plugins: [
